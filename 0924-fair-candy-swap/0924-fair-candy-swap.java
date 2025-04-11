7e2ebe89ce7d325a83f93b4f2826c3cebbb7094b
Rithika0912 <rithikanavanee@gmail.com>
2025-04-11 10:26:18 +0530

    Time: 9 ms (70.36%), Space: 46.1 MB (30.08%) - LeetHub

---
+++ 0924-fair-candy-swap/0924-fair-candy-swap.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = 0, sumB = 0;
+        for(int i=0;i<aliceSizes.length;i++){
+            sumA+=aliceSizes[i];
+        }
+        for(int i=0;i<bobSizes.length;i++){
+            sumB+=bobSizes[i];
+        }
+        int avg = (sumB - sumA)/2;
+        HashSet<Integer> hs = new HashSet<>();
+        for (int b : bobSizes) {
+            hs.add(b);
+        }
+        for (int a : aliceSizes) {
+            int b = a + avg;
+            if (hs.contains(b)) {
+                return new int[]{a, b};
+            }
+        }
+        return new int[0];
+    }
+}
