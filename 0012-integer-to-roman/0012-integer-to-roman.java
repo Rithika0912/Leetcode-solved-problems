6e3c840efa2c65deda73cbe087e42e12468c263f
Rithika0912 <rithikanavanee@gmail.com>
2025-04-21 10:01:51 +0530

    Time: 7 ms (18.3%), Space: 44.9 MB (24.08%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,12 +1,24 @@
 class Solution {
     public String intToRoman(int num) {
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        HashMap<Integer, String> map = new LinkedHashMap<>();
+         map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
         StringBuilder roman = new StringBuilder();
-        for (int i = 0; i < values.length; i++) {
-         while (num >= values[i]) {
-        roman.append(symbols[i]);
-        num -= values[i];
+        for (int value:map.keySet()) {
+         while (num >= value) {
+        roman.append(map.get(value));
+        num -= value;
             }
         }
         
